45131344d206eed56f2bc19e9e816bfda65f1d7f
saikrishnagajavelli <31737129+saikrishnagajavelli@users.noreply.github.com>
2017-09-13 14:01:58 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -14,6 +14,7 @@ import java.util.Date;
 public class StudentGroup implements StudentArrayOperation {
 
 	private Student[] students;
+	ArrayList ar=new ArrayList(Arrays.asList(Student));
 	
 	/**
 	 * DO NOT remove or change this constructor, it will be used during task check
@@ -26,84 +27,163 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getStudents() {
 		// Add your implementation here
+		
+		
+			if(ar.contains(null))
+				{
+				throw new illegalArgumentException();
+				}
+			
+			
+			
+			System.out.println(ar);
+			
+           
 		return null;
 	}
 
 	@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
+		ar.
+		
+			
+		
+				
+				
+			
 	}
 
 	@Override
 	public Student getStudent(int index) {
 		// Add your implementation here
+		
+		if(ar.contains(index))
+		{
+		System.out.println(ar.get(student));
+		}
+		
 		return null;
 	}
 
 	@Override
 	public void setStudent(Student student, int index) {
 		// Add your implementation here
+		
+		
 	}
 
 	@Override
 	public void addFirst(Student student) {
 		// Add your implementation here
+		
+	ar.addFirst(student);
 	}
+	
 
 	@Override
 	public void addLast(Student student) {
 		// Add your implementation here
+		
+	ar.addLast(student);
+	
 	}
 
 	@Override
 	public void add(Student student, int index) {
 		// Add your implementation here
+		
+	ar.addAt(index,student);
 	}
 
 	@Override
 	public void remove(int index) {
 		// Add your implementation here
+		
+	ar.remove(index);
 	}
 
 	@Override
 	public void remove(Student student) {
 		// Add your implementation here
+		
+	ar.remove(student);
 	}
 
 	@Override
 	public void removeFromIndex(int index) {
 		// Add your implementation here
+		
+	for(int i=index;i<ar.size();i++)
+	{
+	ar.remove(index);
+	}
 	}
 
 	@Override
 	public void removeFromElement(Student student) {
 		// Add your implementation here
+		
+	int t=ar.get(student);
+	for(int i=t;i<ar.size();i++)
+	{
+	ar.remove(t);
+	}
 	}
 
 	@Override
 	public void removeToIndex(int index) {
 		// Add your implementation here
+		
+		
+	for(int i=index;i<=index;i--)
+	{
+	ar.remove(index);
+	}
+	
 	}
 
 	@Override
 	public void removeToElement(Student student) {
 		// Add your implementation here
+		
+		int index=ar.get(student);
+		for(int i=index;i<=index;i--)
+	{
+	ar.remove(index);
+	}
 	}
 
 	@Override
 	public void bubbleSort() {
 		// Add your implementation here
+		
+	ar.sort();
+		
 	}
 
 	@Override
 	public Student[] getByBirthDate(Date date) {
 		// Add your implementation here
+		
+	
+		
 		return null;
 	}
 
 	@Override
 	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
 		// Add your implementation here
+		
+	int a=ar.get(firstDate);
+	int b=ar.get(lastDate);
+	for(int i=a+1;i<b;i++)
+	{
+	ar.get(i);
+	}
+	
+	
+	
 		return null;
 	}
 
@@ -122,18 +202,24 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getStudentsByAge(int age) {
 		// Add your implementation here
+		
 		return null;
 	}
 
 	@Override
 	public Student[] getStudentsWithMaxAvgMark() {
 		// Add your implementation here
+		
+	
 		return null;
 	}
 
 	@Override
 	public Student getNextStudent(Student student) {
 		// Add your implementation here
+		
+	int n=ar.get(student);
+	System.out.println(ar.get(n+1));
 		return null;
 	}
 }
